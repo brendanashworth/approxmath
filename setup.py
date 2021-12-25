31a1017ac935b721755eb8d77c24d45687dc441d
Brendan Ashworth <brendan.ashworth@me.com>
2021-12-24 20:22:40 -0800

    show readme on pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 from distutils.core import setup, Extension
 import numpy as np
 
+# render README.md on pypi
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name="approxmath",
-      version="1.0.0",
+      version="1.0.1",
       description="Fast approximate math functions: log, exp, sin, cos",
       author="Brendan Ashworth",
       author_email="brendan.ashworth@me.com",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       ext_modules=[Extension(
           'approxmath', ['src/approxmath/approxmath.c'],
           extra_compile_args=["-Ofast", "-march=native", "-ffast-math"],
